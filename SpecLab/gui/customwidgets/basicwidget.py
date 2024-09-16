d8bcf86eaebb50dfd6cd47982b851fa180239c8c
Jaysterr <j4ysterr@gmail.com>
2024-09-15 22:39:03 -0700

    minor changes

--- SpecLab/gui/customwidgets/basicwidget.py
+++ SpecLab/gui/customwidgets/basicwidget.py
@@ -2,7 +2,10 @@ from PyQt6.QtWidgets import *
 from PyQt6.QtCore import *
 from PyQt6.QtGui import *
 
-
+"""
+Since We can do the style.qss file, We might not actually need this wrapper class? 
+But I'll leave it for now.
+"""
 class BasicWidget(QWidget):
     """
     A wrapper class for a widget. If we want to apply a consistent style
@@ -11,11 +14,11 @@ class BasicWidget(QWidget):
 
     def __init__(self):
         super(BasicWidget, self).__init__()
-        #self.setStyleSheet("border: 3px solid; fill: black")
 
+        #self.setStyleSheet("border: 3px solid; fill: black")
         # sets background color to gray as default
         self.setAutoFillBackground(True)
-
-        palette = self.palette()
-        palette.setColor(QPalette.ColorRole.Window, QColor("gray"))
-        self.setPalette(palette)
+        #
+        # palette = self.palette()
+        # palette.setColor(QPalette.ColorRole.Window, QColor("gray"))
+        # self.setPalette(palette)
