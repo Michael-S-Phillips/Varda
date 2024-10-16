3a889c6f8cbfbcf4970f019d2d601a8b6f97cc0b
Hamad-Ayaz <hamadayaz@arizona.edu>
2024-10-15 17:44:45 -0700

    Update project files for SpecLab-GUI-Hamad

---
+++ SpecLab-GUI-Hamad/src/setup.py
@@ -0,0 +1,5 @@
+# This was something I read about to get python to recognize modules.
+# Turns out it might not be necessary but just gonna leave here for now anyway.
+from setuptools import setup, find_packages
+
+setup(name='SpecLab', version='0.1', packages=find_packages())
