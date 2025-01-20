6d2457095f8832c71f2b3b68610e489296f91982
emmae04 <119893663+emmae04@users.noreply.github.com>
2025-01-20 13:50:10 -0700

    added roi setup files

---
+++ src/features/image_view_roi/roi_view.py
@@ -0,0 +1,56 @@
+# standard library
+from typing import override
+
+# third-party imports
+import pyqtgraph as pg
+from PyQt6.QtWidgets import QVBoxLayout, QWidget
+from PyQt6.QtCore import Qt, QTimer
+
+# local imports
+from features.shared.selection_controls import StretchSelector
+from .band_viewmodel import BandViewModel
+
+
+class ROIView(QWidget):
+    """A view for viewing and selecting ROIs that have been drawn
+    on the main rasterview"""
+
+    viewModel: ROIViewModel
+    widgetHeight = 300
+    updateTimer: QTimer
+
+    def __init__(self, viewModel=None, parent=None):
+        super().__init__(parent)
+        self.setWindowTitle("ROIs for image " + viewModel.imageIndex)
+        self.setWindowFlags(Qt.WindowType.Window | Qt.WindowType.WindowStaysOnTopHint)
+        self.setMaximumHeight(self.widgetHeight)
+        self.viewModel = viewModel
+
+        self._initUI()
+        self._connectSignals()
+        self.show()
+
+    def _initUI(self):
+
+        # Create GraphicsLayout
+        graphicsLayout = pg.GraphicsLayout()
+        graphicsLayout.setContentsMargins(0, 0, 0, 0)
+        graphicsLayout.setSpacing(0)
+
+        # ViewBox setup
+        vbox = pg.ViewBox()
+        # set up the view box
+
+    def _connectSignals(self):
+        self.viewModel.sigBandChanged.connect(self._onBandChanged)
+        self.bandSelector.currentIndexChanged.connect(self.viewModel.selectBand)
+        self.rBandSlider.sigPositionChanged.connect(
+            lambda: self.viewModel.updateBand(r=self.rBandSlider.value())
+        )
+        self.gBandSlider.sigPositionChanged.connect(
+            lambda: self.viewModel.updateBand(g=self.gBandSlider.value())
+        )
+        self.bBandSlider.sigPositionChanged.connect(
+            lambda: self.viewModel.updateBand(b=self.bBandSlider.value())
+        )
+
---
+++ src/features/image_view_roi/roi_viewmodel.py
@@ -0,0 +1,72 @@
+# third party imports
+from PyQt6.QtCore import QObject, pyqtSignal, QTimer
+
+# local imports
+from core.data import ProjectContext
+
+
+class ROIViewModel(QObject):
+    """Simple ViewModel for ROI table.
+
+    Handles all the logic/interaction with the ProjectContext.
+    """
+
+    #sigBandChanged = pyqtSignal(int, int, int)
+
+    def __init__(self, proj: ProjectContext, imageIndex, parent=None):
+        super().__init__(parent)
+        self.proj = proj
+        self.imageIndex = imageIndex
+        self._connectSignals()
+
+
+    def _connectSignals(self):
+        self.proj.sigDataChanged.connect(self._handleDataChanged)
+
+    # selectROI
+
+    # getSelectedROI
+
+    # loadROI data
+
+    # handle ROI change (from project context)
+
+    # def selectBand(self, bandIndex):
+    #     """selects a new band from the image."""
+    #     self.bandIndex = bandIndex
+    #     self.sigBandChanged.emit()
+
+    # def getSelectedBand(self):
+    #     """requests the band corresponding to bandIndex, and returns it."""
+    #     return self.proj.getImage(self.imageIndex).band[self.bandIndex]
+
+    # def getBandCount(self):
+    #     """gets the number of band values in the image."""
+    #     return self.proj.getImage(self.imageIndex).metadata.bandCount - 1
+
+    # def updateBand(self, r=None, g=None, b=None):
+    #     """Begins a debounced band update. Since the slider value is constantly
+    #     changing when being moved, this waits until the change is complete"""
+    #     self._pendingBandValues = (
+    #         int(r) if r else None,
+    #         int(g) if g else None,
+    #         int(b) if b else None,
+    #     )
+    #     if not self.isDragging:
+    #         self.isDragging = True
+    #         self.updateTimer.start(20)
+
+    # def _commitBandUpdate(self):
+    #     """Commits the debounced slider values to the ProjectContext."""
+    #     r, g, b = self._pendingBandValues
+    #     self.proj.updateBand(self.imageIndex, self.bandIndex, r=r, g=g, b=b)
+    #     self.isDragging = False
+
+    # def _handleDataChanged(self, index, changeType):
+    #     """receives ProjectContext updates. Check if the update pertains to us."""
+    #     if index != self.imageIndex:
+    #         return
+    #     if changeType is not ProjectContext.ChangeType.BAND:
+    #         return
+    #     r, g, b = self.proj.getImage(self.imageIndex).band[self.bandIndex].toList()
+    #     self.sigBandChanged.emit(r, g, b)
