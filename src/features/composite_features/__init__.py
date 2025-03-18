031ba9e089eb4789adc85849e210e72e6a5ef9d6
Jaysterr <j4ysterr@gmail.com>
2025-03-18 12:23:53 -0700

    Revert "create composite feature folder"
    This reverts commit 2bd4422bc0a13aea28735073cb763cb725768956.

--- src/features/composite_features/__init__.py
+++
@@ -1,6 +0,0 @@
-"""Composite features combine the basic features we've created into comprehensive workflows.
-
-These should basically act as shells to contain other features. Which simply link their signals as needed to function.
-Their logic should not extend beyond serving this purpose.
-If they do, that functionality should probably be seperated into its own feature
-"""
