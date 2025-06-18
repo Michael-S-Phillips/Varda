47906c57f2b298f39462b2e978f8fde36c937a43
Michael-S-Phillips <phillipsm@arizona.edu>
2025-06-18 12:34:09 -0700

    updated band view stuff so that changed bands are reflected by slider position and label

--- src/features/image_view_band/band_viewmodel.py
+++ src/features/image_view_band/band_viewmodel.py
@@ -5,6 +5,8 @@ from PyQt6.QtCore import QObject, pyqtSignal, QTimer, pyqtSlot
 # local imports
 from core.data import ProjectContext
 from core.entities import Metadata
+import logging
+logger = logging.getLogger(__name__)
 
 
 class BandViewModel(QObject):
@@ -118,13 +120,18 @@ class BandViewModel(QObject):
         if self._ignoreProjectUpdates:
             # if we were the one that caused the update, ignore it
             self._ignoreProjectUpdates = False
+            logger.debug(f"BandViewModel: Ignoring self-generated update for image {index}")
             return
 
         if index != self.imageIndex:
+            logger.debug(f"BandViewModel: Ignoring update for different image {index} (we are {self.imageIndex})")
             return
         if changeType is not ProjectContext.ChangeType.BAND:
+            logger.debug(f"BandViewModel: Ignoring non-band change type {changeType}")
             return
+        
         r, g, b = self.proj.getImage(self.imageIndex).band[self.bandIndex].toList()
+        logger.debug(f"BandViewModel: Emitting band changed signal with r={r}, g={g}, b={b} for band {self.bandIndex}")
         self.sigBandChanged.emit(r, g, b)
 
     def getMetadata(self):
