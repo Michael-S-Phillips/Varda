8277ab7da67e0b4b03b3629db2ca6c4027e2b2ce
Jaysterr <j4ysterr@gmail.com>
2025-07-02 14:48:44 -0700

    fix main thing

--- src/varda/main.py
+++ src/varda/main.py
@@ -1,6 +1,12 @@
 """Entry point to launch the Varda application."""
 
-if __name__ == "__main__":
+
+def main():
+    """Main entry point for the Varda application."""
     import varda
 
     varda.app.initVarda()
+
+
+if __name__ == "__main__":
+    main()
