81a72d4d94ff374bc6848389999723823640c18e
Jaysterr <j4ysterr@gmail.com>
2025-10-19 21:12:13 -0700

    delete unfinished thing

--- src/varda/workflow_spectral_and_param_cubes/ui.py
+++
@@ -1,8 +0,0 @@
-from PyQt6.QtWidgets import QWidget
-
-from varda.image_rendering.raster_view.viewport import ImageViewport
-
-class SpectralCubeAndParamCubeWorkflow(QWidget):
-    def __init__(self, parent=None):
-        super().__init__(parent)
-        self.viewport1 = ImageViewport()
