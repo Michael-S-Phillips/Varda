bb66c96ee4e6896ca8e343e79ac6553f6e2a383a
Jaysterr <j4ysterr@gmail.com>
2026-03-10 22:08:15 -0700

    delete files in '_old' folder, and also the project folder, as it is deprecated

--- src/varda/project/project.py
+++
@@ -1,9 +0,0 @@
-from PyQt6.QtCore import QObject
-
-from varda.common.entities.Image_1 import Image
-
-
-class Project(QObject):
-    def __init__(self, parent=None):
-        super().__init__(parent)
-        self.images: list[Image] = []
