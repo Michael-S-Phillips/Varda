2b6d320ea8eff2768eecca2fa32c53dce2247e47
Jaysterr <j4ysterr@gmail.com>
2026-05-05 11:43:55 -0700

    fix accidental regression in main.py

--- src/varda/main.py
+++ src/varda/main.py
@@ -10,59 +10,35 @@ from PyQt6.QtGui import QPixmap
 from PyQt6.QtWidgets import QApplication, QSplashScreen
 
 import varda
+from varda._actions import MENUBAR
 from varda.app import VardaApplication
-from varda._actions import registerAllActions, MENUBAR
-from varda._actions._context_keys import IMAGE_COUNT
-from varda.common.observable_list import ImageList
 from varda.maingui import MainGUI
 
 
 def initVarda() -> None:
     """Initialize and start the Varda application."""
 
-    ### Initialize PyQt Application ###
     q_app = QApplication(sys.argv)
     q_app.setApplicationName("Varda")
     splash = QSplashScreen(QPixmap("resources/logo.svg"))
     splash.show()
     q_app.processEvents()
 
-    ### Initialize Logging ###
     varda.log._initializeFullLogging()
 
-    ### Set Configurations ###
     pg.setConfigOptions(imageAxisOrder="row-major")
     varda.log.debug("Configurations set")
 
-    ### Create Application ###
     app = VardaApplication()
+    app.pluginManager.hook.onLoad(app=app)
 
-    ### Register DI Providers ###
-    app.injection_store.register_provider(lambda: app.images, ImageList)
-    app.injection_store.register_provider(lambda: app.maingui, MainGUI)
-
-    ### Initialize Expression Context ###
-    app.context[IMAGE_COUNT] = 0
-
-    ### Register Actions ###
-    registerAllActions(app)
-
-    ### Load Plugins ###
-    app.pm.hook.onLoad(app=app)
-
-    ### Build GUI ###
     app.maingui = MainGUI(app=app)
     app.maingui.setModelMenuBar(MENUBAR)
+    app.context.changed.connect(
+        lambda _keys: app.maingui.menuBar().update_from_context(app.context)
+    )
     app.maingui.showMaximized()
 
-    ### Connect Context Updates ###
-    def onImagesChanged(items: list) -> None:
-        app.context[IMAGE_COUNT] = len(items)
-        app.maingui.menuBar().update_from_context(app.context)
-
-    app.images.sigDataChanged.connect(onImagesChanged)
-
-    ### Initialization Complete ###
     varda.log.info("Varda initialized successfully!")
 
     splash.finish(app.maingui)
